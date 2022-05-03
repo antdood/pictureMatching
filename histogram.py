c5be6990f260a62c73b19844cf3a24d0b4d9269d
Anthony Wong <antawck@gmail.com>
2022-05-03 20:51:10 +0800

    pixel colour bucket allocation

--- histogram.py
+++ histogram.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 max_colour_scale = 255
-buckets = 10
+bucket_count = 10
 
 im = Image.open("image.jpg", 'r')
 width, height = im.size
@@ -17,3 +17,20 @@ def generate_bucket_boundaries(max_scale, bucket_count):
 
 	return base_buckets + bigger_buckets
 
+def generate_rgb_bucket_allocation(pixels, bucket_boundaries):
+	buckets = [[0 for _ in range(len(bucket_boundaries))] for _ in range(3)]
+
+	for pixel in pixels:
+		for colour_index, colour_value in enumerate(pixel):
+			buckets[colour_index][get_bucket_index(colour_value, bucket_boundaries)] += 1
+
+	return buckets
+
+
+def get_bucket_index(value, bucket_boundaries):
+	for index, boundary in enumerate(bucket_boundaries):
+		if value < boundary:
+			return index
+	return 0
+	
+print(generate_rgb_bucket_allocation(pixel_values, generate_bucket_boundaries(max_colour_scale, bucket_count)))
