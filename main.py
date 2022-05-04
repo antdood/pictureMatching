b4b6d76f9ae4ca6094e79d6c093a7fd3e905a3ed
Anthony Wong <antawck@gmail.com>
2022-05-04 14:36:01 +0800

    main matching pair generation

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import pathlib
 from histogramImageComparer import isSimilar
+import itertools
 
 image_folder_name = "images"
 
 p = pathlib.Path('.')
 
-images = list(p.glob(f'**/{image_folder_name}/*'))
+images = p.glob(f'**/{image_folder_name}/*')
+
+def generate_matching_pairs(images):
+	image_combinations = itertools.combinations(images, 2)
+
+	matched_images = {}
+
+	for combination in image_combinations:
+		if (combination[0] in matched_images) or (combination[1] in matched_images):
+			continue
+
+		if isSimilar(*combination):
+			matched_images[combination[1]] = combination[0]
+
+	return matched_images
+
 
